2cda68d12dc6e2b3368664ca0d4b17fba993b6a2
조성호 <joseongho@joseonghoui-MacBookAir.local>
2023-05-25 18:11:10 +0900

    ADD: signup

--- Backend/git-test/app.js
+++ Backend/git-test/app.js
@@ -51,27 +51,6 @@ app.post("/users/signup", async (req, res) => {
   );
 });
 
-app.post("/users/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await myDataSource.query(
-    `
-    SELECT
-      users.id
-    FROM
-      users
-    WHERE
-      users.email = ?
-   `,
-    [email]
-  );
-
-  if (!user) {
-    res.json({ message: "SIGNUP_REQUIRED" });
-  }
-
-  return res.json({ userId: user.id });
-});
-
 app.listen(PORT, () => {
   myDataSource
     .initialize()
